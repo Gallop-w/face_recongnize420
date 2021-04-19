a317a083a4790c4f040c6f85be4cfc8862fdde4a
Kevin <18273366580@163.com>
2021-04-19 19:14:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ print(__doc__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
 
-###############################################################################
+###############################################################################edit at 4.19
 # Download the data, if not already on disk and load it as numpy arrays
 
 lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
